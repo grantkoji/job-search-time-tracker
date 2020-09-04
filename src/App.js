eb347c279f725210b6fd7172197809c7dc0cc4f9
grantkoji <grantkoji@gmail.com>
2020-09-03 23:44:23 -0400

    with routes added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App">
+      <img src={require('./images/gameBannerText.png')} className="imageBanner" alt="Home banner" />
+        <Navbar />
+      <Switch>
+        <Route exact path='/' render={(routerprops) => <HomePage {...routerprops}  />}/> 
+        <Route exact path='/resume' render={(routerprops) => <Resume {...routerprops}  />}/> 
+        
+        <Route exact path='/quaranteams' render={(routerprops) => <QuaranTeams  {...routerprops}  />}/> 
+        <Route exact path='/nameem' render={(routerprops) => <NameEm {...routerprops}  />}/> 
+        <Route exact path='/draftprojector' render={(routerprops) => <DraftProjector {...routerprops}  />}/> 
+        <Route exact path='/superhire' render={(routerprops) => <SuperHire {...routerprops}  />}/> 
+        
+        <Route exact path='/blogs' render={(routerprops) => <Blogs {...routerprops}  />}/>  
+        <Route exact path='/aboutme' render={(routerprops) => <AboutMe {...routerprops}  />}/>  
+        <Route exact path='/fantasyfootball' render={(routerprops) => <Hobbies {...routerprops}  />}/>  
+        <Route exact path='/timegoals' render={(routerprops) => <TimeTracker {...routerprops} />} /> 
+        <Route exact path='/todayschedule' render={(routerprops) => <TodaySchedule {...routerprops} />} /> 
+        <Route exact path='/schedulearchives' render={(routerprops) => <ScheduleArchives  {...routerprops} />} /> 
+        <Route exact path='/timecharts' render={(routerprops) => <TimeCharts {...routerprops}  />}/>
+      </Switch>
+
+      <br/><br/><br/><br/><br/><br/><br/><br/>
+      <Footer />
+    </div>
     </div>
   );
 }
